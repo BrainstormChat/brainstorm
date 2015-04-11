a2b0e469676c7c3d9a407a2a893cf3afffec192d
Sergio Berlotto <sergio.berlotto@gmail.com>
2015-04-11 17:45:23 -0300

    Save msg and usr added

--- api/doodle.js
+++ api/doodle.js
@@ -3,15 +3,22 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var port = process.env.PORT || 8888;
+var db = require("./chatdatabase")
 
 server.listen(port, function () {
   console.log('Server listening at port %d', port);
 });
 
 io.on('connection',function(socket){
-	console.info('conectado');
+	console.info('+1 conectado');
 	socket.on('sendMessage',function(mensagem){
-		console.info('recebeu sendMessage');
+        console.info('recebeu sendMessagem:' + mensagem.msg);
 		socket.emit('newMessage',mensagem);
+        db.gravaMsg(mensagem.room, mensagem.msg, mensagem.user, function(){
+            console.log("inseriu msg no banco!")
+        })
 	});
+    socket.on('identuser', function(mensagem){
+        db.gravaUsr(userid, useremail);
+    });
 });
