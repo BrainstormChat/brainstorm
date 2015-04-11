1119e60831c9b08a99dda86d7df2dce517bdbba1
Sergio Berlotto <sergio.berlotto@gmail.com>
2015-04-11 15:07:53 -0300

    Database in doodle

--- api/doodle.js
+++ api/doodle.js
@@ -1,6 +1,14 @@
 (function(){
 
+    var databaseuri = "mongodb://db:db@ds041160.mongolab.com:41160/brainstormchat"
+    var collections = ["teams", "tokens", "users", "chatsession"]
+    var db = require('mongojs').connect(databaseuri, collections)
+
 	var io = require('socket.io')(8080);
+
+    var gravaMsg = function(text, owner, sessionid){
+        db.chatsession.find({"identifier": sessionid})
+    }
 
 	io.on('helloworld',function(socket){
 		console.log(socket);
