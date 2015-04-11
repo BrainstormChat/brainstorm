ad1cbb7b42807dcf1a176b4c10853a8aaf9b378c
Mar <gfmarster#gmail.com>
2015-04-11 22:17:29 +0000

    capando fora o angular

--- js/controller_input_message.js
+++ js/controller_input_message.js
@@ -1,20 +1,15 @@
-
-
     window.app.controller('input_message', function inputMessage ($scope) {
       $scope.send = function(){
 
         if ($scope.message) {
 
             socket.emit('sendMessage',{
-                "room" : "dsv", 
-                "user" : "teste-frontend",
+                "room" : "dsv",
+                "user" : localStorage.getItem('username'),
                 "msg" : $scope.message,
                 "time" : new Date().getTime()
             });
 
-            /*
-            socket.on('newMesssage', function(msg){})
-            */
         }
 
     }
--- js/controller_messages.js
+++ js/controller_messages.js
@@ -1,50 +1,26 @@
-window.app.controller('messages', function messages ($scope) {
 
-    $scope.chat = [{
-        user: "colocado na mão",
-        message: "mensagem amarrada no código fonte",
-        time: "7 minutes ago",
-        user_initials: "CNM",
-        counter: 1
-      },{
-          user: "colocado na mão",
-          message: "mensagem amarrada no código fonte",
-          time: "7 minutes ago",
-          user_initials: "CNM",
-          counter: 2
-      },{
-          user: "colocado na mão",
-          message: "mensagem amarrada no código fonte",
-          time: "7 minutes ago",
-          user_initials: "CNM",
-          counter: 3
-    }]
+var imputMessageInChat = function inputMessageInChat (data)
+{
+    var li = document.createElement('li');
+    li.className = "left clearfix";
+
+    li.innerHTML = '<span class="chat-img pull-left"><img src="http://placehold.it/50/55C1E7/fff&amp;text=' + data.user_initials + '" alt="User Avatar" class="img-circle"></span><div class="chat-body clearfix"><div class="header"><strong class="primary-font">' + data.user + '</strong><small class="pull-right text-muted"><span class="glyphicon glyphicon-time"></span>' + data.time + '</small></div><p>' + data.message + '</p></div>';
+
+    document.getElementById('chat-wall').appendChild(li);
+}
 
 
-    window.socket.on('newMessage', function(data){
+window.socket.on('newMessage', function(data){
 
-        var received = {};
+    var received = {};
 
-        received['user'] = data.user;
-        received['message'] = data.msg;
+    received['user'] = data.user;
+    received['message'] = data.msg;
 
-        var time = new Date(data.time);
-        received['time'] = time.getHours() + ':' + time.getMinutes() + ':' + time.getSeconds();
+    var time = new Date(data.time);
+    received['time'] = time.getHours() + ':' + time.getMinutes() + ':' + time.getSeconds();
 
-        var name = data.user.split(' '),
-            initials = '';
-        for (var i=0; i<name.length; ++i) {
-          initials += name[i].substring(0,1);
-        }
-        received['user_initials'] = initials;
-        received['counter'] = data.time;
+    received['user_initials'] = data.user.substring(0,3);;
 
-        $scope.chat.push(received);
-
-        console.log('#####');
-        console.log(data);
-        console.log('#####');
-    });
-
-
+    imputMessageInChat(received);
 });
