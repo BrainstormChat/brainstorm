7f892d3c7e2bb7dbf65a8c9a75e6c43a1bf8947c
Christian <guilherme@opiniaoearte.com.br>
2015-04-11 17:03:58 -0300

    Criado .emit(newCitation)

--- api/doodle.js
+++ api/doodle.js
@@ -8,10 +8,25 @@ server.listen(port, function () {
   console.log('Server listening at port %d', port);
 });
 
+var helper = {};
+helper.formatCitation(_msg,_signal){
+	_msg.type = _signal;
+	return _msg;
+}
+
 io.on('connection',function(socket){
 	console.info('conectado');
 	socket.on('sendMessage',function(mensagem){
 		console.info('recebeu sendMessage');
 		socket.emit('newMessage',mensagem);
+		if(mensagem.msg.indexOf('#')){
+			socket.emit('newCitation',helper.formatCitation(mensagem,"#"));
+		}
+		if(mensagem.msg.indexOf("@")){
+			socket.emit('newCitation',helper.formatCitation(mensagem,"@"));
+		}
+		if(mensagem.msg.indexOf('$')){
+			socket.emit('newCitation',helper.formatCitation(mensagem,"$"));
+		}
 	});
 });
