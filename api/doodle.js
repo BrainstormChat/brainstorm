b35632fdcbd23884e7622c2bdb272c5483e6aaad
Christian <guilherme@opiniaoearte.com.br>
2015-04-11 16:04:22 -0300

    Adicionando console.log no doodle.js

--- api/doodle.js
+++ api/doodle.js
@@ -4,7 +4,9 @@
 	var io = require('socket.io')(8888);
 
 	io.on('connetion',function(socket){
+		console.info('conectado');
 		socket.on('sendMessage',function(mensagem){
+			console.info('recebeu sendMessage');
 			socket.broadcast('newMessage',mensagem);
 		});
 	});
