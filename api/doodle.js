ac815844aefebe0b729cf2be8ffbec5c763b90ad
Sergio Berlotto <sergio.berlotto@gmail.com>
2015-04-11 15:39:38 -0300

    Adjust port for tsuru

--- api/doodle.js
+++ api/doodle.js
@@ -1,7 +1,7 @@
 (function(){
     var db = require('./database');
 
-	var io = require('socket.io')(8080);
+	var io = require('socket.io')(8888);
 
 	io.on('connetion',function(socket){
 		socket.on('sendMessage',function(mensagem){
