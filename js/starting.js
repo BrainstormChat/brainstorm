e50fbb0afb1264d702dcd31937c613ae06d26b1a
Mar <gfmarster#gmail.com>
2015-04-12 00:41:00 +0000

    citations and login user

--- js/starting.js
+++ js/starting.js
@@ -9,18 +9,23 @@ $(document).ready(function(){
             $("#username").val(localStorage.getItem("username"));
         }
     }
-    
+
     $('#usernameModal').on('shown.bs.modal', function (e) {
         $("#username").focus();
     })
-    
+
     $("#enter").bind("click", function(){
         localStorage.setItem("username", $("#username").val());
         $("#usernameModal").modal('hide');
         $(".container-fluid.fade").addClass("in");
+
+        window.socket.emit('identuser',{
+            'user': $("#username").val(), //string
+            'email': 'email.do.usuario@server.com', //string
+        });
     })
-    
-    
+
+
     $('#username').keypress(function(e) {
        if(e.which == 13) {
          $('#enter').click();
