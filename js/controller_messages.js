4a15dae9e419f8a2e63a6bcbdd52c2fa9493f76f
Mar <gfmarster#gmail.com>
2015-04-12 10:00:57 +0000

    filtros funcionando

--- js/controller_messages.js
+++ js/controller_messages.js
@@ -4,11 +4,13 @@ var imputMessageInChat = function inputMessageInChat (data)
     var li = document.createElement('li');
     li.className = "left clearfix";
 
-    li.innerHTML = '<span class="chat-img pull-left"><img src="http://placehold.it/50/55C1E7/fff&amp;text=' + data.user_initials + '" alt="User Avatar" class="img-circle"></span><div class="chat-body clearfix"><div class="header"><strong class="primary-font">' + data.user + '</strong><small class="pull-right text-muted"><span class="glyphicon glyphicon-time"></span>' + data.time + '</small></div><p>' + data.message + '</p></div>';
+    li.innerHTML = '<span class="chat-img pull-left"><img src="http://placehold.it/50/55C1E7/fff&amp;text=' + data.user_initials + '" alt="User Avatar" class="img-circle"></span><div class="chat-body clearfix"><div class="header"><strong class="primary-font">' + data.user + '</strong><small class="pull-right text-muted"><span class="glyphicon glyphicon-time"></span>' + data.time + '</small></div><p class="msg">' + data.message + '</p></div>';
 
     document.getElementById('chat-wall').appendChild(li);
 
     $(".panel-body")[0].scrollTop = $(".panel-body")[0].scrollHeight;
+
+    window.bs.applyFilter();
 }
 
 var imputMessagePrepare = function imputMessagePrepare (data){
